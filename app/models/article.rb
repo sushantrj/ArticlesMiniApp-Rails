dc30a4bf5326737ff498845ce86c287e7b807741
sushantrj <rjbhanot38@gmail.com>
2020-08-05 15:32:45 +0530

    Added associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   #This is model created for Articles
   #we can access the data using getters and setters
+  belongs_to :user
   validates :title, presence: true, length: {minimum: 5, max: 20}
   validates :description, presence: true, length: {minimum: 10, max: 300}
 end
